1bc8c712beff83d29975b5c56147cf3eafc58cc7
skbangari <skbangari@deakin.edu.au>
2020-04-27 16:03:21 +1000

    removed mountains, added animation to boat

---
+++ Assets/Scripts/ExitGame.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ExitGame : MonoBehaviour
+{
+    public void ExitThisGame()
+    {
+        UnityEngine.Debug.LogError("ExitGame");
+        Application.Quit();
+    }
+}
