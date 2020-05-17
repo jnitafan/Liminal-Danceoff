0cb7df048d5ac3e395d5e74f2c54d2e426cb6aa9
Bunedicto <bfili@deakin.edu.au>
2020-05-18 01:12:45 +1000

    Added birds and 3D Models
    3D Models from the environment team added to the project. Along with the addition of birds to the environment, flying in the background.

---
+++ Assets/Scripts/BirdsMovement.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BirdsMovement : MonoBehaviour
+{
+    public float flySpeed = 1.0f;
+    public float time = 0.0f;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        if (time<10)
+        {
+            flyForward();
+            time = time + Time.deltaTime;
+        }
+        if (time>10)
+        {
+            turnAround();
+            time = time + Time.deltaTime;
+        }
+        if (time>30)
+        {
+            time = 0.0f;
+        }
+    }
+
+    void flyForward()
+    {
+        transform.Translate(flySpeed * Vector3.forward * Time.deltaTime);
+
+    }
+
+    void turnAround()
+    {
+        transform.Translate((flySpeed/2) * Vector3.forward * Time.deltaTime);
+        transform.Rotate(0.0f, 0.01f*flySpeed, 0.0f);
+
+    }
+}
