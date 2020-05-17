2047bdd0843ba830b3890e47ff6217a43eefb625
Jacob <jnitafan@deakin.edu.au>
2020-05-17 21:47:49 +1000

    Changed audio to not include birds, and added some d a y 'n' n i g h t scripts. waiting on ben to fine tune the script for better values

--- Assets/Scripts/TimeofDayCycle.cs
+++ Assets/Scripts/TimeofDayCycle.cs
@@ -1,18 +1,64 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
+using UnityEngine.Rendering.PostProcessing;
 
 public class TimeofDayCycle : MonoBehaviour
 {
-    // Start is called before the first frame update
+    PostProcessVolume m_Volume;
+    // Vignette m_Vignette;
+    ColorGrading m_ColorGrading;
+
+    public int cycleTime;
+
+    [DisplayName("LiftDay")]
+    [Trackball(TrackballAttribute.Mode.Lift)]
+    public Vector4Parameter liftDay;
+
+    [DisplayName("LiftNight")]
+    [Trackball(TrackballAttribute.Mode.Lift)]
+    public Vector4Parameter liftNight;
+
+    [DisplayName("GammaDay")]
+    [Trackball(TrackballAttribute.Mode.Gamma)]
+    public Vector4Parameter gammaDay;
+    [DisplayName("GammaNight")]
+    [Trackball(TrackballAttribute.Mode.Gamma)]
+    public Vector4Parameter gammaNight;
+
+    [DisplayName("GainDay")]
+    [Trackball(TrackballAttribute.Mode.Gain)]
+    public Vector4Parameter gainDay;
+    [DisplayName("GainDay")]
+    [Trackball(TrackballAttribute.Mode.Gain)]
+    public Vector4Parameter gainNight;
+
+    Vignette m_Vignette;
+
     void Start()
     {
-        
+
+        m_ColorGrading = ScriptableObject.CreateInstance<ColorGrading>();
+        m_ColorGrading.enabled.Override(true);
+        m_ColorGrading.lift.Override(liftDay);
+        m_ColorGrading.gamma.Override(gammaDay);
+        m_ColorGrading.gain.Override(gainDay);
+
+        m_Vignette = ScriptableObject.CreateInstance<Vignette>();
+        m_Vignette.enabled.Override(true);
+        m_Vignette.intensity.Override(1f);
+
+        m_Volume = PostProcessManager.instance.QuickVolume(gameObject.layer, 100f, m_ColorGrading);
+        m_Volume = PostProcessManager.instance.QuickVolume(gameObject.layer, 100f, m_Vignette);
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        m_ColorGrading.lift.value = Vector4.Lerp(liftDay, liftNight, Mathf.Sin(time * Time.realtimeSinceStartup));
+        m_ColorGrading.gamma.value = Vector4.Lerp(gammaDay, gammaNight, Mathf.Sin(time * Time.realtimeSinceStartup));
+        m_ColorGrading.gain.value = Vector4.Lerp(gainDay, gainNight, Mathf.Sin(time * Time.realtimeSinceStartup));
+    }
+
+    void OnDestroy()
+    {
+        RuntimeUtilities.DestroyVolume(m_Volume, true, true);
     }
 }
