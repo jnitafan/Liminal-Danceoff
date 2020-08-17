d9d3a4c63d0e93033545ce8101be73588962edf8
Jacob <Jnitafan@deakin.edu.au>
2020-08-17 16:42:33 +1000

    Added in all the assets from blender. Rendering wall lightmaps inside blender instead of using unity, might take a day or two. You all can start the programming jobs now.

---
+++ Assets/Scripts/TileMatChange.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TileMatChange : MonoBehaviour
+{
+    public bool changeColor = true;
+    public Material[] randomTileMat;
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (changeColor)
+        {
+            for (int i = 0; i < 24; i++)
+            {
+                GameObject child = transform.GetChild(i).gameObject;
+                child.GetComponent<MeshRenderer>().material = randomTileMat[Random.Range(0, randomTileMat.Length)];
+                changeColor = false;
+                StartCoroutine(Timer());
+            }
+        }
+    }
+    IEnumerator Timer()
+    {
+        yield return new WaitForSeconds(30 * Time.deltaTime);
+        changeColor = true;
+    }
+}
