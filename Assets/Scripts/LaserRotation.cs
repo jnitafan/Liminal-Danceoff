73128fdf32a8ae3f3615c76e15c3b4804165a8c4
KOSTAS LITSIS <litsisc@deakin.edu.au>
2020-08-21 19:12:00 +1000

    Finished laser machines and their movement script. Also added the soundwaves for the speakers.

---
+++ Assets/Scripts/LaserRotation.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LaserRotation : MonoBehaviour
+{
+    public float xAngle, yAngle, zAngle;
+    public float speed;
+    
+    
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(xAngle * speed, yAngle * speed, zAngle * speed);
+    }
+}
