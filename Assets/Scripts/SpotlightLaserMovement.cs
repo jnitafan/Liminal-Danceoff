1c90c87babf42651d131aeab6488bebe80be247b
James Bolitho <jabolitho@deakin.edu.au>
2020-08-22 09:50:24 +1000

    Added Light sources + scripting for Light sources, edited existing light and colour materials

---
+++ Assets/Scripts/SpotlightLaserMovement.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpotlightLaserMovement : MonoBehaviour
+{
+    float xRotation;
+    float yRotation;
+    float zRotation;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        RotateAroundPoint();
+    }
+    void RotateAroundPoint()
+    {
+        xRotation = Mathf.Cos(Time.deltaTime * 10);
+        yRotation = Mathf.Sin(Time.deltaTime * 10);
+        transform.Rotate(zRotation, xRotation, yRotation, Space.World);
+    }
+}
+
--- Assets/Scripts/TileMatChange.cs
+++ Assets/Scripts/TileMatChange.cs
@@ -4,8 +4,18 @@ using UnityEngine;
 
 public class TileMatChange : MonoBehaviour
 {
+    private float time;
     public bool changeColor = true;
     public Material[] randomTileMat;
+    public GameObject FluroscentLightbulb;
+    public GameObject SpotLightLightBox;
+    private Color[] randomLightColor = { new Color(174, 71, 217), new Color(34, 169, 238), new Color(0, 200, 130), new Color(174, 71, 217) };
+    //private Color LightColor1 = new Color(174, 71, 217);
+    //private Color LightColor2 = new Color(34, 169, 238);
+    //private Color LightColor3 = new Color(0, 200, 130);
+    //private Color LightColor4 = new Color(174, 71, 217);
+    public float speed = 100f;
+    Light LightBoxLight;
     // Start is called before the first frame update
     void Start()
     {
@@ -16,18 +26,51 @@ public class TileMatChange : MonoBehaviour
     {
         if (changeColor)
         {
-            for (int i = 0; i < 24; i++)
+            TileColourChange();
+            FluroscentLightbulbColourChange();
+            SpotLightLightBoxColourChange();
+        }
+    }
+
+    void TileColourChange()
+    {
+            for (int i = 0; i < 40; i++)
             {
                 GameObject child = transform.GetChild(i).gameObject;
                 child.GetComponent<MeshRenderer>().material = randomTileMat[Random.Range(0, randomTileMat.Length)];
                 changeColor = false;
-                StartCoroutine(Timer());
+                StartCoroutine(Timerelement());
             }
-        }
     }
-    IEnumerator Timer()
+    //Dancefloortiles color changing script (uses tile materials,now called light mat materials, located in tile mat materials)
+    void FluroscentLightbulbColourChange()
+    {
+            FluroscentLightbulb.GetComponent<MeshRenderer>().material = randomTileMat[Random.Range(0, randomTileMat.Length)];
+            changeColor = false;
+            StartCoroutine(Timerelement());
+    }
+    //FluroscentLightbulb color changing script (uses tile materials, now called light mat materials, located in tile mat materials)
+    void SpotLightLightBoxColourChange()
+    {
+        /*LightBoxLight = SpotLightLightBox.GetComponent<Light>();
+        time = Mathf.PingPong(Time.time *speed, 1);
+        LightBoxLight.color = Color.Lerp(randomLightColor[Random.Range(0, randomLightColor.Length)], randomLightColor[Random.Range(0, randomLightColor.Length)], time);
+        changeColor = false;
+        StartCoroutine(Timerelement());*/
+        LightBoxLight = SpotLightLightBox.GetComponent<Light>();
+        LightBoxLight.color = randomLightColor[Random.Range(0, randomLightColor.Length)];
+        changeColor = false;
+        StartCoroutine(Timerelement());
+    }
+    //SpotLightLightBox color change script (uses tile materials, lerps between colour as spotlight rotates around the club)
+    void LaserMachineColourChange()
+    {
+
+    }
+    IEnumerator Timerelement()
     {
         yield return new WaitForSeconds(30 * Time.deltaTime);
         changeColor = true;
     }
+    //Time to be changed and set with BPM of added audio to edit light according to the beat of the song playing.
 }
