ceff13dda03bbbd9db24b634df820d4c29aeb6cb
Jacob <Jnitafan@deakin.edu.au>
2020-08-24 05:00:13 +1000

    Added fog particle systems, added more light sources, modified the lighting script to fit multiple lightsources. TODO: Optimize the scripts better with material modification instead of switching, tweak the lighting better to fit the mood.

--- Assets/Scripts/TileMatChange.cs
+++ Assets/Scripts/TileMatChange.cs
@@ -7,13 +7,12 @@ public class TileMatChange : MonoBehaviour
     private float time;
     public bool changeColor = true;
     public Material[] randomTileMat;
-    public GameObject FluroscentLightbulb;
-    public GameObject SpotLightLightBox;
-    private Color[] randomLightColor = { new Color(174, 71, 217), new Color(34, 169, 238), new Color(0, 200, 130), new Color(174, 71, 217) };
-    //private Color LightColor1 = new Color(174, 71, 217);
-    //private Color LightColor2 = new Color(34, 169, 238);
-    //private Color LightColor3 = new Color(0, 200, 130);
-    //private Color LightColor4 = new Color(174, 71, 217);
+    public Material[] randomTileMatMuted;
+    public GameObject[] FluroscentLightbulb;
+    public GameObject[] SpotLightLightBox;
+
+    [ColorUsageAttribute(true, true)]
+    public Color[] randomLightColor = { new Color(174, 71, 217), new Color(34, 169, 238), new Color(0, 200, 130), new Color(174, 71, 217) };
     public float speed = 100f;
     Light LightBoxLight;
     // Start is called before the first frame update
@@ -34,20 +33,23 @@ public class TileMatChange : MonoBehaviour
 
     void TileColourChange()
     {
-            for (int i = 0; i < 40; i++)
-            {
-                GameObject child = transform.GetChild(i).gameObject;
-                child.GetComponent<MeshRenderer>().material = randomTileMat[Random.Range(0, randomTileMat.Length)];
-                changeColor = false;
-                StartCoroutine(Timerelement());
-            }
+        for (int i = 0; i < 40; i++)
+        {
+            GameObject child = transform.GetChild(i).gameObject;
+            child.GetComponent<MeshRenderer>().material = randomTileMatMuted[Random.Range(0, randomTileMatMuted.Length)];
+            changeColor = false;
+            StartCoroutine(Timerelement());
+        }
     }
     //Dancefloortiles color changing script (uses tile materials,now called light mat materials, located in tile mat materials)
     void FluroscentLightbulbColourChange()
     {
-            FluroscentLightbulb.GetComponent<MeshRenderer>().material = randomTileMat[Random.Range(0, randomTileMat.Length)];
+        for (int i = 0; i < FluroscentLightbulb.Length; i++)
+        {
+            FluroscentLightbulb[i].GetComponent<MeshRenderer>().material = randomTileMat[Random.Range(0, randomTileMat.Length)];
             changeColor = false;
             StartCoroutine(Timerelement());
+        }
     }
     //FluroscentLightbulb color changing script (uses tile materials, now called light mat materials, located in tile mat materials)
     void SpotLightLightBoxColourChange()
@@ -57,10 +59,14 @@ public class TileMatChange : MonoBehaviour
         LightBoxLight.color = Color.Lerp(randomLightColor[Random.Range(0, randomLightColor.Length)], randomLightColor[Random.Range(0, randomLightColor.Length)], time);
         changeColor = false;
         StartCoroutine(Timerelement());*/
-        LightBoxLight = SpotLightLightBox.GetComponent<Light>();
-        LightBoxLight.color = randomLightColor[Random.Range(0, randomLightColor.Length)];
-        changeColor = false;
-        StartCoroutine(Timerelement());
+
+        for (int i = 0; i < SpotLightLightBox.Length; i++)
+        {
+            LightBoxLight = SpotLightLightBox[i].GetComponent<Light>();
+            LightBoxLight.color = randomLightColor[Random.Range(0, randomLightColor.Length)];
+            changeColor = false;
+            StartCoroutine(Timerelement());
+        }
     }
     //SpotLightLightBox color change script (uses tile materials, lerps between colour as spotlight rotates around the club)
     void LaserMachineColourChange()
