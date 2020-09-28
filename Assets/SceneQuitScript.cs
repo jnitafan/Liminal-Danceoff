9d1252099d1f9ea27864a7617a9a17c93349eafd
James Bolitho <jabolitho@deakin.edu.au>
2020-09-28 18:06:07 +1000

    Merge branch 'master' of https://bitbucket-students.deakin.edu.au/scm/dimm-ug/liminalvr-ug-squad_2020t2 into master
    # Conflicts:
    #	Assets/DanceitOff.unity
    
    Added, ending script
    
    
    Former-commit-id: 571c7688c0fa87e73a9825017a3912bf507c596b

---
+++ Assets/SceneQuitScript.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Liminal.SDK.Core;
+using Liminal.Core.Fader;
+
+
+public class SceneQuitScript : MonoBehaviour
+{
+    private void Start()
+    {
+        Debug.Log("is running");
+        var fader = ScreenFader.Instance;
+        fader.FadeTo(Color.black, 10f);
+        ExperienceApp.End();
+        Debug.Log("is running");
+
+
+    }
+
+}
