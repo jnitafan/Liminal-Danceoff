b0c26a92b23202f6e765c8427b6b31011606257c
James Bolitho <jabolitho@deakin.edu.au>
2020-09-28 18:20:47 +1000

    Implemented the EndingScene script, needs to undergo further bug resolution by Kostas or Jacob
    Former-commit-id: 0e0312b5e852e508c64bb5151e99a18e2df0f38f

---
+++ Assets/SceneEnding.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Liminal.SDK.Core;
+using Liminal.Core.Fader;
+
+public class SceneEnding : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("Is running");
+        var fader = ScreenFader.Instance;
+        fader.FadeTo(Color.black, 10f);
+        ExperienceApp.End();
+        Debug.Log("Is running");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
